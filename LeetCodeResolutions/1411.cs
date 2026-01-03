e34bfbf3e8a6e4e2c367bed8f83fa203fd92c214
Renan Costa <91814882+renanzitoo@users.noreply.github.com>
2026-01-02 23:10:12 -0300

    add: 3 new leetcode resolutions

---
+++ LeetCodeResolutions/1411.cs
@@ -0,0 +1,21 @@
+﻿public class Solution1411
+{
+    private const int MOD = 1_000_000_007;
+
+    public int NumOfWays(int n)
+    {
+        long dpA = 6; 
+        long dpB = 6; 
+
+        for (int i = 2; i <= n; i++)
+        {
+            long newDpA = (2 * dpA + 2 * dpB) % MOD;
+            long newDpB = (2 * dpA + 3 * dpB) % MOD;
+
+            dpA = newDpA;
+            dpB = newDpB;
+        }
+
+        return (int)((dpA + dpB) % MOD);
+    }
+}
---
+++ LeetCodeResolutions/961.cs
@@ -0,0 +1,12 @@
+﻿public class Solution961 {
+    public int RepeatedNTimes(int[] nums) {
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in nums){
+            if(!seen.Add(num)) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
